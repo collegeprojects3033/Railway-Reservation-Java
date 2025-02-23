03bfe1a852ac0e26d6f07a4408c52713aa3a4903
Team SuperChargers <collegeprojects3033@gmail.com>
2025-02-23 14:42:32 +0000

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -1,50 +1,47 @@
-import java.io.*;
- class RailwaySystem {
-    
+class RailwaySystem {
     String train_list[];
     String reservations[];
- 
 }
-class trian {
-    String tname;
-    int tno;
-    String source;
-    String details;
-    int sets;
 
+class Train {
+    String TrainName;
+    int TrainNo;
+    String Source;
+    String Details;
+    String Destination;
+    int Sets;
+}
 
-    
+class Reservation {
+    int ReservationID;
+    int Passengers;
+    String TrainName;
+    int SeatNumber;
+    int JourneyDate;
 }
-class Reservation
-{
-    int reservationID;
-    int passengers;
-    String train_name;
-    int seat_number;
-    int date_of_journey;
-}
-class pasenger{
-    String name;
-    int age;
-    int mobile_number;
-    pasenger(String n,int a,int m)
-    {
-        name=n;
-        age=a;
-        mobile_number=m;
+
+class Passenger {
+    String PassengerName;
+    int PassengerAge;
+    int MobileNo;
+    Passenger(String n, int a, int m) {
+        PassengerName = n;
+        PassengerAge = a;
+        MobileNo = m;
     }
-    void pasenger_info_dip()
-    {
-        System.out.println("Name:"+name);
-        System.out.println("Age:"+age);
-        System.out.println("Mobile number:"+mobile_number);
+
+    void ShowPassengerInfo() {
+        System.out.println("Name:" + PassengerName);
+        System.out.println("Age:" + PassengerAge);
+        System.out.println("Mobile number:" + MobileNo);
     }
 }
+
 public class Main {
-    
+
     public static void main(String[] args) {
-         pasenger p=new pasenger("ganesh",18,1234567890);
-        p.pasenger_info_dip();
+        Passenger p = new Passenger("ganesh", 18, 1234567890);
+        p.ShowPassengerInfo();
     }
-    
+
 }
